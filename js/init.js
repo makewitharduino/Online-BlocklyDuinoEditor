9f469d132687378a98d23c0cf900fa9c0143a256
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2015-05-01 10:46:34 +0900

    remove compilerflasher.js

--- js/init.js
+++ js/init.js
@@ -330,22 +330,6 @@ function setScript() {
   }
 }
 
-$(document).ready(function () {
-  compilerflasher = new compilerflasher(getFiles);
-  compilerflasher.on("pre_verify", function () {
-    $("#event_msg").append('pre_verify event fired!<br/><br/>')
-    $("#debug_arduino").html('verification');
-  });
-  compilerflasher.on("verification_succeed", function (binary_size) {
-    $("#event_msg").append('verification_succeed event fired! Sketch size: ' + binary_size + "<br/><br/>")
-    $("#debug_arduino").html('Verification Complete : ' + binary_size);
-  });
-  compilerflasher.on("verification_failed", function (error_output) {
-    $("#event_msg").append("verification_failed event fired! \nCompilation error: <pre>" + error_output + "</pre><br/><br/>")
-    $("#debug_arduino").html(error_output);
-  });
-});
-
 function getFiles() {
   // return {"sketch.ino": Blockly.Generator.workspaceToCode('Arduino') }
   //$('textarea#textarea_arduino').val() //&lt; et &lt;
@@ -369,8 +353,7 @@ function change_lang(obj){
   window.location = loc.protocol + '//' + loc.host + loc.pathname + '?lang=' + val;
 }
 
-function openSample(){
-  var url = 'http://lets.makewitharduino.com';
+function openNewTab(url){
   window.open(url,'_blank');
 }
 
